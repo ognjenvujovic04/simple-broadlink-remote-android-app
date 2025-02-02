08f6ba00c158bb9603eb46fd32b9a2a0a0e2dcd9
ognjenvujovic04 <143551253+ognjenvujovic04@users.noreply.github.com>
2025-02-02 14:58:11 +0100

    Add python broadlink script for testing

---
+++ python_test.py
@@ -0,0 +1,32 @@
+import broadlink
+from time import sleep
+
+# device = broadlink.hello('192.168.1.10')
+# if device:
+#     print("Hello device found via broadcast discovery")
+#     print(f"Device found: {device}")
+#     print(f"IP Address: {device.host[0]}")
+#     print(f"MAC Address: {device.mac.hex()}")
+#     device.enter_learning()
+#     # wait for 3 seconds
+#     sleep(3)
+#     packet = device.check_data()
+#     print(f"Device codes: {packet}")
+
+devices = broadlink.discover(timeout=5, local_ip_address='192.168.1.6')
+
+if devices:
+    for device in devices:
+        device.auth()
+        print(f"Device found: {device}")
+        print(f"IP Address: {device.host[0]}")
+        print(f"MAC Address: {device.mac.hex()}")
+        device.enter_learning()
+        # wait for 3 seconds
+        sleep(3)
+        packet = device.check_data()
+        print(f"Device codes: {packet}")
+        device.send_data(packet)
+        
+else:
+    print("No devices found via broadcast discovery. Trying unicast discovery...")
